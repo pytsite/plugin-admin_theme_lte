aa25da1685048f7b96fec1333bc73ea7c897f26e
Oleksandr Shepetko <a@shepetko.com>
2018-10-29 08:27:36 +0200

    Bugfix 2.2.1

--- __init__.py
+++ __init__.py
@@ -5,11 +5,25 @@ __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 
+def _build_assets():
+    from plugins import assetman
+
+    assetman.build(__name__)
+
+
 def plugin_load_wsgi():
     """Hook
     """
+    from pytsite import plugman
     from plugins import admin
     from ._theme import Theme
 
     # Register admin theme
     admin.register_theme(Theme())
+
+    # Events handlers
+    plugman.on_install_all(_build_assets)
+
+
+def plugin_install():
+    _build_assets()
